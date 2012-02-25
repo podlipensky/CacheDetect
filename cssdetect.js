6f7b45498b62dfaba6f6d93cd9707657423ceb23
podlipensky <podlipensky@gmail.com>
2012-02-25 15:16:46 -0800

    Watch for the moment when CSS file is loaded (for second time), so we can do the comparison

--- cssdetect.js
+++ cssdetect.js
@@ -10,7 +10,7 @@ function isCacheEnabled() {
     var w = div.offsetWidth;
     // request the same cachedetect.js by adding <script> tag dynamically to <header>
     var head = document.getElementsByTagName("head")[0];
-    //find existing stylesheet and remove it
+    //find existing stylesheet and remove it (trying to force IE to reload the resource for second time)
     links = head.getElementsByTagName('link');
     var len = links.length;
     for (var i = 0; i < len; i++) {
@@ -19,16 +19,39 @@ function isCacheEnabled() {
             break;
         }
     }
-        
+    var newStylesheetIndex = document.styleSheets.length;
+    var cssLoadedCallback = function () { //store div and its current width in the closure
+        console.log('cache: ' + (w != div.offsetWidth ? 'disabled (fresh)' : 'enabled (from cache)'));
+    };
     var link = document.createElement('link');
     link.type = "text/css";
     link.rel = "stylesheet";
     link.href = "cssdetect.ashx";
+    link.onload = cssLoadedCallback;
+    link.onreadystatechange = function () { if (this.readyState == 'loaded' || this.readyState == 'complete') cssLoadedCallback(); }
     head.appendChild(link);
+    runCallbackForStylesheet(newStylesheetIndex, cssLoadedCallback);
+}
 
-    window.setTimeout(function () {
-        console.log('cache: ' + (w != div.offsetWidth ? 'disabled (fresh)' : 'enabled (from cache)'));
-    }, 1000);
+function runCallbackForStylesheet(index, callback) {//watch for the moment when requested css file is loaded
+    try {
+        if (document.styleSheets[index].cssRules) {//FF and Safari support
+            callback();
+        } else {
+            if (document.styleSheets[index].rules && document.styleSheets[index].rules.length) {
+                callback();
+            } else {
+                setTimeout(function () {
+                    runCallbackForStylesheet(index);
+                }, 250);
+            }
+        }
+    }
+    catch (e) {
+        setTimeout(function () {
+            runCallbackForStylesheet(index);
+        }, 250);
+    }
 }
 
 isCacheEnabled();
